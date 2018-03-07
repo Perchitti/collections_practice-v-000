40d5457ff7d6dfec74f3d8b17b53f007e62a6860
Lauren Perchitti <laurenperchitti@gmail.com>
2018-03-07 18:30:55 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -6,3 +6,24 @@ end
 def sort_array_desc(array)
   array.sort.reverse
 end
+
+def sort_array_char_count(array)
+end
+
+def swap_elements(array)
+end
+
+def reverse_array(array)
+end
+
+def kesha_maker(array)
+end
+
+def find_a(array)
+end
+
+def sum_array(array)
+end
+
+def add_s(array)
+end
